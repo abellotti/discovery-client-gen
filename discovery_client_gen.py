5bf1b3817cf205c93171970de61f24124182e84d
Alberto Bellotti <abellott@redhat.com>
2022-05-02 08:42:40 -0400

    Adding support for optional user supplied openapi schema. re-enabled inventory client gen.

--- discovery_client_gen.py
+++ discovery_client_gen.py
@@ -12,16 +12,17 @@ from pathlib import Path
 import os
 import json
 import requests
+import shutil
 import sys
 
-OPENAPI_SOURCE = (
-    "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli"
-)
+OPENAPI_SOURCE = "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli"
 OPENAPI_VERSION = "5.4.0"
 CLIENT_LANG = "python"
 
 
-def gen_client(base_dir, client_name, openapi_url, package_name):
+def gen_client(
+    base_dir, client_name, package_name, openapi_url=None, openapi_schema_dir=None
+):
     Path(base_dir).mkdir(parents=True, exist_ok=True)
     print("--------------------------------------------------------------")
     print("Base directory:                  ", base_dir)
@@ -44,13 +45,23 @@ def gen_client(base_dir, client_name, openapi_url, package_name):
     Path(client_dir).mkdir(parents=True, exist_ok=False)
     print("Client directory:                ", client_dir)
 
-    resp = requests.get(openapi_url)
-    if resp.status_code != 200:
-        raise Exception("Failed to download " + openapi_url)
     openapi_file = f"{client_dir}/openapi.json"
-    with open(openapi_file, "w") as of:
-        of.write(json.dumps(json.loads(resp.content), indent=4))
-    print("Downloaded ", openapi_url)
+    if openapi_schema_dir:
+        schema_override = f"{openapi_schema_dir}/{client_name}_openapi.json"
+        if Path(client_dir).exists():
+            shutil.copy(schema_override, openapi_file)
+            print("Using ", schema_override)
+
+    if not Path(openapi_file).exists():
+        if not openapi_url:
+            raise Exception("Must specify a url to download " + openapi_file)
+
+        resp = requests.get(openapi_url)
+        if resp.status_code != 200:
+            raise Exception("Failed to download " + openapi_url)
+        with open(openapi_file, "w") as of:
+            of.write(json.dumps(json.loads(resp.content), indent=4))
+        print("Downloaded ", openapi_url)
 
     print("")
     print("Using OpenAPI Generator CLI Jar: ", jar_path)
@@ -75,26 +86,29 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise Exception("Usage: discovery_client_gen.py target_directory")
     client_dir = sys.argv[1]
+    script_dir = os.path.dirname(__file__)
+    openapi_schema_dir = f"{script_dir}/openapi_schemas"
     # Ingress for uploading & registering new device
     gen_client(
-        client_dir,
-        "ingress",
-        "https://console.redhat.com/api/ingress/v1/openapi.json",
-        "ingress_client",
+        base_dir=client_dir,
+        client_name="ingress",
+        package_name="ingress_client",
+        openapi_url="https://console.redhat.com/api/ingress/v1/openapi.json",
     )
     # Swatch, RHSM Subscriptions, pulling model analog, etc.
     gen_client(
-        client_dir,
-        "rhsm_subscriptions",
-        "https://console.redhat.com/api/rhsm-subscriptions/v1/openapi.json",
-        "rhsm_subscriptions_client",
+        base_dir=client_dir,
+        client_name="rhsm_subscriptions",
+        package_name="rhsm_subscriptions_client",
+        openapi_url=(
+            "https://console.redhat.com/api/rhsm-subscriptions/v1/openapi.json"
+        ),
     )
-    """
     # HBI, Host Based Inventory
     gen_client(
-        client_dir,
-        "inventory",
-        "https://console.redhat.com/api/inventory/v1/openapi.json",
-        "inventory_client",
+        base_dir=client_dir,
+        client_name="inventory",
+        package_name="inventory_client",
+        openapi_schema_dir=openapi_schema_dir,
+        openapi_url="https://console.redhat.com/api/inventory/v1/openapi.json",
     )
-    """
